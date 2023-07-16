adda9f315436ca5f037ce181951c632ce2f21bc2
John Titor <1165137798@qq.com>
2023-07-16 16:52:05 +0800

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 apt install unrar
 sudo apt-get install golang
-
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install flax
 pip install albumentations einops tqdm matplotlib jax-smi
