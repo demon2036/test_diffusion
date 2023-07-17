9c059f5d02766abe00e96aefeaac41781a876225
john titor <1165137798@qq.com>
2023-07-18 02:17:23 +0800

    commit

--- test_google.py
+++ test_google.py
@@ -1,21 +1,41 @@
 import json
+import os
+
 import requests
+import argparse
 
 
-token='ya29.a0AbVbY6PKpLRTGOBrrLD9a65oAnjgFzav4-4tyr-UAs3xnQrAHm5CUfVjHf_zjJ5m3MI8VrZvPNxsD6N5TTE-_02VMbRr7oDZtUnZYkFOfAiBOYiRSRjivE-utSH4FpRCpj_xZ9Es2xccNr5w02BwEr-6ZFkBaCgYKAUgSARASFQFWKvPlMICO2Xi_sqnzZ2J4WJQ5ig0163'
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', default='./check_points')
+parser.add_argument('-tk', '--token', )
+parser.add_argument('-cn', '--code_name', )
+args = parser.parse_args()
+print(args)
+
 headers = {
-    "Authorization": f"Bearer {token}"
+    "Authorization": f"Bearer {args.token}"
 }
 
 para = {
-    "name": "check.rar",
-    #"parents": [""]
+    "name": f"{args.code_name}.rar",
 }
+
+os.system(f'rm -rf ./{args.code_name}.rar')
+
+last_file=sorted(os.listdir(f'{args.path}'),key=lambda x:int(x))[-1]
+last_file_path=f'{args.path}/{last_file}'
+
+
+os.system(f'rar a ./{args.code_name}.rar {last_file_path}')
+
 
 files = {
     'data': ('metadata', json.dumps(para), 'application/json;charset=UTF-8'),#charset=UTF-8
-    'file': open('./check.rar', 'rb')
+    'file': open(f'./{args.code_name}.rar', 'rb')
 }
+
+
+
 
 r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
                   headers=headers,
