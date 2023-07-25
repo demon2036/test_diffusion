be6060dd53d28c9506a75c3b9ad9e6e325313732
john titor <1165137798@qq.com>
2023-07-26 04:18:37 +0800

    commit

---
+++ test_google2.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', default='./check_points')
+parser.add_argument('-cn', '--code_name', )
+args = parser.parse_args()
+print(args)
+
+para = {
+    "name": f"{args.code_name}.rar",
+}
+
+os.system(f'rm -rf ./{args.code_name}.rar')
+
+last_file=sorted(os.listdir(f'{args.path}'),key=lambda x:int(x))[-1]
+last_file_path=f'{args.path}/{last_file}'
+print(last_file_path)
+
+os.system(f'rar a ./{args.code_name}.rar {last_file_path}')
+
+os.system(f'gsutil cp  {args.code_name}.rar  gs://jw4-bucket-1/{args.code_name}.rar')
