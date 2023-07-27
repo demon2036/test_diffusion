8ed52775a2d6f7fd322118a6db54c7d1eb4f05fe
john titor <1165137798@qq.com>
2023-07-28 04:09:40 +0800

    commit

---
+++ modules/gaussian/gaussianDecoder.py
@@ -0,0 +1,83 @@
+from modules.gaussian.gaussian import Gaussian, extract, ModelPrediction
+import jax
+from tqdm import tqdm
+import numpy as np
+import jax.numpy as jnp
+
+
+class GaussianDecoder(Gaussian):
+    def __init__(
+            self,
+            *args,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+
+    """
+    def p_sample_loop(self, key, params, shape, x_self_cond=None):
+        key, normal_key = jax.random.split(key, 2)
+        img = self.generate_nosie(normal_key, shape)
+
+        x_start = None
+        for t in tqdm(reversed(range(0, self.num_timesteps)), total=self.num_timesteps):
+            key, normal_key = jax.random.split(key, 2)
+            img, x_start = self.p_sample(normal_key, params, img, t, x_self_cond)
+
+        ret = img
+        if self.predict_residual:
+            ret += x_self_cond
+        return ret
+
+    def sample(self, key, state, lr_image):
+
+        b, h, w, c = lr_image.shape
+        lr_image = jax.image.resize(lr_image, (b, h * self.sr_factor, w * self.sr_factor, c), method='bicubic')
+        noise_shape = lr_image.shape
+        print(noise_shape)
+        # if self.num_timesteps > self.sampling_timesteps:
+        #     return self.ddim_sample(key, noise_shape, lr_image)
+        # else:
+        #     return self.p_sample_loop(key, params, noise_shape, lr_image)
+        res = self.ddim_sample(key, state, lr_image, noise_shape)
+        if self.predict_residual:
+            res += lr_image
+
+        return res
+
+    # # def sample(self, key, params, lr_image):
+    # #     b, h, w, c = lr_image.shape
+    # #     lr_image = jax.image.resize(lr_image, (b, h * self.sr_factor, w * self.sr_factor, c), method='bicubic')
+    # #     noise_shape = lr_image.shape
+    # #
+    # #     if self.num_timesteps > self.sampling_timesteps:
+    # #         return self.ddim_sample(key, noise_shape, lr_image)
+    # #     else:
+    # #         return self.p_sample_loop(key, params, noise_shape, lr_image)
+    # """
+
+    def p_loss(self, key, state, params, x_start, t):
+        noise = self.generate_nosie(key, shape=x_start.shape)
+
+        x = self.q_sample(x_start, t, noise)
+        model_output = state.apply_fn({"params": params}, x, t, x_start)
+
+        if self.objective == 'predict_noise':
+            target = noise
+        elif self.objective == 'predict_x0':
+            target = x_start
+        elif self.objective == 'predict_v':
+            v = self.predict_v(x_start, t, noise)
+            target = v
+        else:
+            target = None
+
+        p_loss = self.loss(target, model_output)
+
+        p_loss = (p_loss * extract(self.loss_weight, t, p_loss.shape)).mean()
+        return p_loss
+
+    def __call__(self, key, state, params, img):
+        key_times, key_noise = jax.random.split(key, 2)
+        b, h, w, c = img.shape
+        t = jax.random.randint(key_times, (b,), minval=0, maxval=self.num_timesteps)
+
+        return self.p_loss(key_noise, state, params, img, t)
--- modules/models/unet.py
+++ modules/models/unet.py
@@ -5,9 +5,11 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 from typing import *
+
+from modules.models.autoencoder import Encoder
 from modules.models.transformer import Transformer
 from modules.models.embedding import SinusoidalPosEmb
-from modules.models.resnet import ResBlock, DownSample,UpSample
+from modules.models.resnet import ResBlock, DownSample, UpSample
 
 
 class Unet(nn.Module):
@@ -18,13 +20,20 @@ class Unet(nn.Module):
     dim_mults: Sequence = (1, 2, 4, 8)
     dtype: Any = jnp.bfloat16
     self_condition: bool = False
+    use_encoder: bool = False
+    encoder_configs: Any = None
 
     @nn.compact
     def __call__(self, x, time, x_self_cond=None, *args, **kwargs):
 
+        if self.use_encoder :
+            x_self_cond=Encoder(**self.encoder_configs)(x_self_cond)
+            x_self_cond=jax.image.resize(x_self_cond,x.shape,'bicubic')
+
+
         if x_self_cond is not None and self.self_condition:
             x = jnp.concatenate([x, x_self_cond], axis=3)
-        elif self.self_condition :
+        elif self.self_condition:
             x = jnp.concatenate([x, jnp.zeros_like(x)], axis=3)
         print(x.shape)
 
@@ -77,6 +86,7 @@ class Unet(nn.Module):
         x = nn.Conv(self.out_channels, (1, 1), dtype="float32")(x)
         return x
 
+
 class MultiUnet(nn.Module):
     dim: int = 32
     out_channels: int = 3
@@ -96,7 +106,7 @@ class MultiUnet(nn.Module):
             'channels': self.channels,
             'dim_mults': self.dim_mults,
             'dtype': self.dtype,
-            'self_condition':self.self_condition
+            'self_condition': self.self_condition
         }
         x = Unet(**unet_configs)(x, time, x_self_cond)
 
@@ -105,15 +115,11 @@ class MultiUnet(nn.Module):
         return x
 
 
-
-
-
-
 class UVit(nn.Module):
     dim: int = 384
-    patch :int =2
+    patch: int = 2
     out_channels: int = 3
-    depth: int =12
+    depth: int = 12
     # resnet_block_groups: int = 8,
     dtype: Any = jnp.bfloat16
     self_condition: bool = False
@@ -123,7 +129,7 @@ class UVit(nn.Module):
 
         if x_self_cond is not None and self.self_condition:
             x = jnp.concatenate([x, x_self_cond], axis=3)
-        elif self.self_condition :
+        elif self.self_condition:
             x = jnp.concatenate([x, jnp.zeros_like(x)], axis=3)
         print(x.shape)
 
@@ -135,20 +141,20 @@ class UVit(nn.Module):
             nn.Dense(time_dim, dtype=self.dtype)
         ])(time)
 
-        x = nn.Conv(self.dim, (self.patch, self.patch), (self.patch, self.patch),padding="SAME", dtype=self.dtype)(x)
+        x = nn.Conv(self.dim, (self.patch, self.patch), (self.patch, self.patch), padding="SAME", dtype=self.dtype)(x)
         r = x
 
         h = []
 
-        for _ in range(self.depth//2):
-            x=Transformer(self.dim,self.dtype)(x)
+        for _ in range(self.depth // 2):
+            x = Transformer(self.dim, self.dtype)(x)
             h.append(x)
 
         for _ in range(self.depth // 2):
-            x=jnp.concatenate([x,h.pop()],axis=-1)
-            x=nn.Dense(self.dim,dtype=self.dtype)(x)
+            x = jnp.concatenate([x, h.pop()], axis=-1)
+            x = nn.Dense(self.dim, dtype=self.dtype)(x)
             x = Transformer(self.dim, self.dtype)(x)
 
-        x=einops.rearrange(x,'b h w (c p1 p2)->b (h p1) (w p2) c',p1=self.patch,p2=self.patch)
+        x = einops.rearrange(x, 'b h w (c p1 p2)->b (h p1) (w p2) c', p1=self.patch, p2=self.patch)
         x = nn.Conv(self.out_channels, (3, 3), dtype="float32")(x)
         return x
