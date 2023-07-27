5f4c9f266a104ad654a965a438f3fd9da1351af5
john titor <1165137798@qq.com>
2023-07-27 21:25:22 +0800

    commit

--- modules/models/autoencoder.py
+++ modules/models/autoencoder.py
@@ -3,20 +3,18 @@ from typing import *
 from modules.models.unet_block import DecoderUpBlock, EncoderDownBlock
 
 
-
 class Encoder(nn.Module):
     dims: Sequence
-    num_blocks: int
-    dtype: str
-    latent: int
+    num_blocks: int =2
+    dtype: Any = 'bfloat16'
+    latent: int = 4
     use_attn: bool = False
-
 
     @nn.compact
     def __call__(self, x, *args, **kwargs):
         x = nn.Conv(self.dims[0], (7, 7), dtype=self.dtype, padding="same")(x)
         for i, dim in enumerate(self.dims):
-            x = EncoderDownBlock(dim, self.num_blocks, True if i != len(self.dims) else False,dtype=self.dtype,)(x)
+            x = EncoderDownBlock(dim, self.num_blocks, True if i != len(self.dims) else False, dtype=self.dtype, )(x)
         x = nn.Conv(self.latent, (1, 1), dtype=self.dtype)(x)
         x = nn.tanh(x)
         return x
@@ -32,7 +30,7 @@ class Decoder(nn.Module):
     def __call__(self, x, *args, **kwargs):
         x = nn.Conv(self.dims[0], (7, 7), dtype=self.dtype, padding="same")(x)
         for i, dim in enumerate(self.dims):
-            x = DecoderUpBlock(dim, self.num_blocks, True if i != len(self.dims) else False,dtype=self.dtype,)(x)
+            x = DecoderUpBlock(dim, self.num_blocks, True if i != len(self.dims) else False, dtype=self.dtype, )(x)
         x = nn.Conv(3, (3, 3), dtype='float32', padding='SAME')(x)
         x = nn.tanh(x)
         return x
@@ -43,14 +41,12 @@ class AutoEncoder(nn.Module):
     num_blocks: int = 2
     dtype: str = 'bfloat16'
     latent: int = 3
+
     # use_attn:bool =False
 
     @nn.compact
     def __call__(self, x, *args, **kwargs):
         x = Encoder(self.dims, self.num_blocks, self.dtype, self.latent)(x)
         reversed_dims = list(reversed(self.dims))
-        x = Decoder(reversed_dims, self.num_blocks, self.dtype,)(x)
+        x = Decoder(reversed_dims, self.num_blocks, self.dtype, )(x)
         return x
-
-
-
