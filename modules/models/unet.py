bad6dabd2110f64d51a1fcf92afa5a60d717001b
john titor <1165137798@qq.com>
2023-07-29 01:16:18 +0800

    fix autoencoder bug

--- modules/models/unet.py
+++ modules/models/unet.py
@@ -30,9 +30,9 @@ class Unet(nn.Module):
             n = 2 ** 3
             x_self_cond = Encoder(**self.encoder_configs)(x_self_cond)
             x_self_cond = nn.Conv(3 * n ** 2, (5, 5), padding="SAME", dtype=self.dtype)(x_self_cond)
-            x = einops.rearrange('b h w (c p1 p2)->b (h p1) (w p2) c', p1=n, p2=n)
+            x_self_cond = einops.rearrange(x_self_cond,'b h w (c p1 p2)->b (h p1) (w p2) c', p1=n, p2=n)
             x_self_cond = jax.image.resize(x_self_cond, x.shape, 'bicubic')
-
+        print(x.shape)
         if x_self_cond is not None and self.self_condition:
             x = jnp.concatenate([x, x_self_cond], axis=3)
         elif self.self_condition:
