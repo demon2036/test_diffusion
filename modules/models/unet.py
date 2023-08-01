3638c1165cd00721a4ae893bda6e8b0b5453bb6e
john titor <1165137798@qq.com>
2023-08-02 02:26:06 +0800

    commit

--- modules/models/unet.py
+++ modules/models/unet.py
@@ -96,7 +96,6 @@ class Unet(nn.Module):
         x = jnp.concatenate([x, r], axis=3)
         x = res_block(dim, dtype=self.dtype)(x, t)
         x = nn.Conv(self.out_channels*self.patch_size**2, (1, 1), dtype="float32")(x)
-
         x=einops.rearrange(x,'b h w (c p1 p2)->b (h p1) (w p2) c')
 
         return x
