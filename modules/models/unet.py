ce122865c6a218ca94f72db5be301ad599fbe126
john titor <1165137798@qq.com>
2023-08-10 22:13:09 +0800

    fix bug of min snr

--- modules/models/unet.py
+++ modules/models/unet.py
@@ -65,6 +65,8 @@ class Unet(nn.Module):
 
         if self.use_encoder:
             n = 2 ** 3
+            if z_rng is None:
+                z_rng=jax.random.PRNGKey(seed=42)
             x_self_cond = AutoEncoderKL(**self.encoder_configs)(x_self_cond,z_rng)
             # x_self_cond = nn.Conv(3 * n ** 2, (5, 5), padding="SAME", dtype=self.dtype)(x_self_cond)
             # x_self_cond = einops.rearrange(x_self_cond, 'b h w (c p1 p2)->b (h p1) (w p2) c', p1=n, p2=n)
