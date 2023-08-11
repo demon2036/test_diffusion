63876d6d45ec176dba0808d0bc5525c2af207169
john titor <1165137798@qq.com>
2023-08-12 01:53:05 +0800

    fix bug

--- modules/models/unet.py
+++ modules/models/unet.py
@@ -67,10 +67,10 @@ class Unet(nn.Module):
             n = 2 ** 3
             if z_rng is None:
                 z_rng = jax.random.PRNGKey(seed=42)
-            x_self_cond = AutoEncoderKL(**self.encoder_configs)(x_self_cond, z_rng)
-            # x_self_cond = nn.Conv(3 * n ** 2, (5, 5), padding="SAME", dtype=self.dtype)(x_self_cond)
-            # x_self_cond = einops.rearrange(x_self_cond, 'b h w (c p1 p2)->b (h p1) (w p2) c', p1=n, p2=n)
-            # x_self_cond = jax.image.resize(x_self_cond, x.shape, 'bicubic')
+            x_self_cond = AutoEncoderKL(**self.encoder_configs).encode(x_self_cond, z_rng)
+            x_self_cond = nn.Conv(3 * n ** 2, (5, 5), padding="SAME", dtype=self.dtype)(x_self_cond)
+            x_self_cond = einops.rearrange(x_self_cond, 'b h w (c p1 p2)->b (h p1) (w p2) c', p1=n, p2=n)
+            x_self_cond = jax.image.resize(x_self_cond, x.shape, 'bicubic')
 
         if x_self_cond is not None and self.self_condition:
             x = jnp.concatenate([x, x_self_cond], axis=3)
