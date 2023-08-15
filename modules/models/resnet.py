92ae4a81c286a46bf5948e93926c21e0a51e0710
john titor <1165137798@qq.com>
2023-08-16 03:16:58 +0800

    commit

--- modules/models/resnet.py
+++ modules/models/resnet.py
@@ -114,6 +114,7 @@ class ResBlock(nn.Module):
         h = Block(dim_out=self.dim_out, dtype=self.dtype)(x, scale_shift=scale_shift)
 
         if cond_emb is not None:
+            cond_emb = nn.silu(cond_emb)
             cond_emb = nn.Dense(self.dim_out * 2, dtype=self.dtype)(cond_emb)
             cond_emb = einops.rearrange(cond_emb, 'b c -> b  1 1 c')
             scale_shift = jnp.split(cond_emb, indices_or_sections=2, axis=3)
