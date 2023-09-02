0fe6939c5e266f469894fb147ed71a23c221c124
john2036 <1165137798@qq.com>
2023-09-03 02:25:29 +0800

    commit

--- train_diff.py
+++ train_diff.py
@@ -4,9 +4,9 @@ from flax.jax_utils import replicate
 from tqdm import tqdm
 import jax.random
 from data.dataset import generator
-from modules.state_utils import create_state, apply_ema_decay, copy_params_to_ema, ema_decay_schedule
-from modules.utils import EMATrainState, create_checkpoint_manager, load_ckpt, read_yaml, update_ema, \
-    sample_save_image_diffusion, get_obj_from_str
+from modules.infer_utils import sample_save_image_diffusion
+from modules.state_utils import create_state, apply_ema_decay, copy_params_to_ema, ema_decay_schedule, EMATrainState
+from modules.utils import  create_checkpoint_manager, load_ckpt, read_yaml, update_ema, get_obj_from_str
 import flax
 import os
 from functools import partial
@@ -20,7 +20,20 @@ initialise_tracking()
 
 os.environ['XLA_FLAGS'] = '--xla_gpu_force_compilation_parallelism=1'
 
+@partial(jax.pmap, static_broadcasted_argnums=(3), axis_name='batch')
+def train_step(state, batch, train_key, cls):
+    def loss_fn(params):
+        loss = cls(train_key, state, params, batch)
+        return loss
 
+    grad_fn = jax.value_and_grad(loss_fn)
+    loss, grads = grad_fn(state.params)
+    #  Re-use same axis_name as in the call to `pmap(...train_step,axis=...)` in the train function
+    grads = jax.lax.pmean(grads, axis_name='batch')
+    new_state = state.apply_gradients(grads=grads)
+    loss = jax.lax.pmean(loss, axis_name='batch')
+    metric = {"loss": loss}
+    return new_state, metric
 
 
 def train():
