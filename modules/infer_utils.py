e44f78c4e380a44de4055987cea70e074a8fe371
demon2036 <1165137798@qq.com>
2023-09-08 22:00:15 +0800

    commit

--- modules/infer_utils.py
+++ modules/infer_utils.py
@@ -113,23 +113,18 @@ def diff_encode(state: EMATrainState, x):
     return state.apply_fn({'params': state.ema_params}, x, method=DiffEncoder.encode)
 
 
-def sample_save_image_sr_eval(key, diffusion: GaussianSR, steps, state: EMATrainState, batch, save_path):
-    os.makedirs(save_path, exist_ok=True)
+def sample_save_image_sr_eval(key, diffusion: GaussianSR, state: EMATrainState, batch):
     b, h, w, c = batch.shape
     lr_image = jax.image.resize(batch, (b, h // diffusion.sr_factor, w // diffusion.sr_factor, c), method='bilinear')
-    os.makedirs(save_path, exist_ok=True)
-    diffusion.set_state(state)
+    diffusion.eval()
     sample = diffusion.sample(key, state, lr_image)
+    diffusion.train()
     sample.append(batch)
     all_image = jnp.concatenate(sample, axis=0)
-    all_image = all_image / 2 + 0.5
-    diffusion.state = None
-    all_image = einops.rearrange(all_image, '(n b) h w c->b c (n h) w', n=len(sample))
-    all_image = np.array(all_image)
-    all_image = torch.Tensor(all_image)
-    save_image(all_image, f'{save_path}/{steps}.png')
+    all_image = einops.rearrange(all_image, '(n b) h w c->b  (n h) w c', n=len(sample))
+    return all_image
 
 
 def sample_save_image_sr(key, diffusion: GaussianSR, state: EMATrainState, lr_image):
-    sample = diffusion.sample(key, state, lr_image,return_img_only=True)
+    sample = diffusion.sample(key, state, lr_image, return_img_only=True)
     return sample
--- trainers/diff_sr_trainer.py
+++ trainers/diff_sr_trainer.py
@@ -4,7 +4,8 @@ from flax.jax_utils import replicate
 from tqdm import tqdm
 import jax.random
 from data.dataset import generator
-from modules.infer_utils import sample_save_image_diffusion, sample_save_image_sr, jax_img_save
+from modules.infer_utils import sample_save_image_diffusion, sample_save_image_sr, jax_img_save, \
+    sample_save_image_sr_eval
 from modules.state_utils import create_state, apply_ema_decay, copy_params_to_ema, ema_decay_schedule, EMATrainState
 from modules.utils import create_checkpoint_manager, load_ckpt, read_yaml, update_ema, get_obj_from_str, default
 import flax
@@ -67,16 +68,23 @@ class DiffSRTrainer(Trainer):
         save_args = orbax_utils.save_args_from_target(model_ckpt)
         self.checkpoint_manager.save(self.finished_steps, model_ckpt, save_kwargs={'save_args': save_args}, force=False)
 
-    def sample(self, sample_state=None, batch=None, save_sample=False, return_sample=False):
+    def sample(self, sample_state=None, batch=None, save_sample=False, return_sample=False, eval=False):
         sample_state = default(sample_state, flax.jax_utils.replicate(self.state))
         batch = default(batch, next(self.dl))
 
         try:
-            sr_img = sample_save_image_sr(self.rng,
-                                          self.gaussian,
-                                          sample_state,
-                                          batch,
-                                          )
+
+            if eval:
+                sr_img = sample_save_image_sr_eval(self.rng,
+                                                   self.gaussian,
+                                                   sample_state,
+                                                   batch, )
+            else:
+                sr_img = sample_save_image_sr(self.rng,
+                                              self.gaussian,
+                                              sample_state,
+                                              batch,
+                                              )
 
             if save_sample:
                 jax_img_save(sr_img, self.save_path, self.finished_steps)
@@ -113,7 +121,7 @@ class DiffSRTrainer(Trainer):
 
                 if self.finished_steps % self.sample_steps == 0:
                     print(self.finished_steps, self.sample_steps)
-                    self.sample(state,save_sample=True)
+                    self.sample(state, save_sample=True)
                     self.state = flax.jax_utils.unreplicate(state)
                     self.save()
 
