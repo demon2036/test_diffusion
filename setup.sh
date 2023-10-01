c879ec8f01189be0f817ad2bc7b15bab908ee6ec
demon2036 <1165137798@qq.com>
2023-10-02 02:01:43 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ apt-get -y --force-yes install golang
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install flax
 pip install albumentations einops tqdm matplotlib jax-smi pytorch_fid
+pip install tensorflow==2.13 tensorflow-datasets webdataset keras-cv
