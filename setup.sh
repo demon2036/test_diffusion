cb5970d8c969a00d6a805144f37df3a128f0f033
demon2036 <1165137798@qq.com>
2023-12-04 21:30:14 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt-get  install golang
 pip install diffusers
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 #pip install flax --ignore-installed PyYAML
-pip install flax --ignore-installed PyYAML
+pip install flax
 pip install albumentations einops tqdm matplotlib jax-smi
 pip install tensorflow==2.13 tensorflow-datasets webdataset keras-cv timm
 
